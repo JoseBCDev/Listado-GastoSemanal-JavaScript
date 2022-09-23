8eae7065cc08d6540b3bdf1ae0e5d72511fec70c
xdgonex <bendezucortezjoseluis@gmail.com>
2022-09-22 19:59:50 -0500

    Agregando color al sueldo Restante y validacion de formulario con sueldo negativo

--- js/app.js
+++ js/app.js
@@ -110,6 +110,27 @@ class UI{
             document.querySelector('#restante').textContent = restante;
         }
         
+        comprobarPresupuesto(presupuestoObj)
+        {
+            const {presupuesto,restante} = presupuestoObj;
+            
+            const restanteDiv = document.querySelector('.restante');
+
+            if((presupuesto/4) > restante)
+            {
+                restanteDiv.classList.remove('alert-success','alert-warning');
+                restanteDiv.classList.add('alert-danger');
+            }else if((presupuesto/2) > restante){
+                restanteDiv.classList.remove('alert-success');
+                restanteDiv.classList.add('alert-warning');
+            }
+
+            if(restante<0)
+            {
+                this.imprimirAlerta('El presupuesto se ha agotado','error')
+                formulario.querySelector('button[type="submit"]').disabled = true;
+            }
+        }
 }
 
 //Instanciar Objetos
@@ -161,5 +182,7 @@ function agregarGasto(e){
     const {gastos,restante} = presupuesto;
     ui.agregarGastoListado(gastos);
 
-     ui.actualizarRestante(restante);
+    ui.actualizarRestante(restante);
+
+    ui.comprobarPresupuesto(presupuesto);
 }
